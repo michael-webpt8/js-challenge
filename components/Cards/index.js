41cae589f9981a07ca59be806bf8c4ae8ca471bf
Michael Williams <manicdesigner@gmail.com>
2019-08-17 13:32:35 -0400

    built function for article

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,51 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios
+	.get('https://lambda-times-backend.herokuapp.com/articles')
+	.then((response) => {
+		console.log(response.data.articles);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+function ArticleCard(article) {
+	// Layout -----------------------------------------------------
+	// <div class="card">
+	//   <div class="headline">{Headline of article}</div>
+	//   <div class="author">
+	//     <div class="img-container">
+	//       <img src={url of authors image} />
+	//     </div>
+	//     <span>By {authors name}</span>
+	//   </div>
+	// </div>
+	// -------------------------------------------------------------
+
+	const card = document.createElement('div');
+	card.classList.add('card');
+
+	const headline = document.createElement('div');
+	headline.classList.add('headline');
+	headline.textContent = article.headline;
+	card.appendChild(headline);
+
+	const author = document.createElement('div');
+	author.classList.add('author');
+	card.appendChild(author);
+
+	imgContainer = document.createElement('div');
+	imgContainer.classList.add('img-container');
+	author.appendChild(imgContainer); // appends inside of author div.
+
+	const image = document.createElement('img');
+	img.src = article.authorPhoto;
+	imgContainer.appendChild(img); // appends inside of author div inside of img-container.
+
+	const authorName = document.createElement('span');
+	authorName.textContent = article.authorName;
+	author.appendChild(authorName); // appends inside of aurtho div
+
+	return card;
+}
