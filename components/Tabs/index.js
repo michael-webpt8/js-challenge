c06fbb09a9bd857915ca1d9b1915a46da9b5e1cc
Michael Williams <manicdesigner@gmail.com>
2019-08-17 13:07:04 -0400

    working on tabs with axios

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,13 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+	.get('https://lambda-times-backend.herokuapp.com/topics')
+	.then((response) => {
+		console.log(response.data.topics);
+		const topicsArr = response.data.topics;
+	})
+	.catch((err) => {
+		console.log(err);
+	});
