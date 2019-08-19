a71b89f539223c8c1e716d177a094be0b805aac9
Michael Williams <manicdesigner@gmail.com>
2019-08-19 19:18:16 -0400

    remove console logs from axios call add for in loop loops object push articles on to array loop in loop to reach content of content build out article cards append to container

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,62 +18,62 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 axios
-	.get('https://lambda-times-backend.herokuapp.com/articles')
-	.then((response) => {
-		console.log('r', response.data.articles);
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    const articles = response.data.articles;
 
-		const articles = response.data.articles;
-        console.log(articles);
-        const articleArr = 
-		for(let i = 0; i < articles.length; i++) {
-			console.log('a', articles[i]);
-		}
-
-		//	console.log(articleArray);
-
-		const article = ArticleCard(articles);
-		console.log('articles', article);
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+    const articleArr = [];
+    for (let article in articles) {
+      articleArr.push(articles[article]);
+    }
+    const container = document.querySelector('.cards-container');
+    articleArr.forEach(article => {
+      article.forEach(content => {
+        const newArticle = ArticleCard(content);
+        container.appendChild(newArticle);
+      });
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 function ArticleCard(article) {
-	// Layout -----------------------------------------------------
-	// <div class="card">
-	//   <div class="headline">{Headline of article}</div>
-	//   <div class="author">
-	//     <div class="img-container">
-	//       <img src={url of authors image} />
-	//     </div>
-	//     <span>By {authors name}</span>
-	//   </div>
-	// </div>
-	// -------------------------------------------------------------
+  // Layout -----------------------------------------------------
+  // <div class="card">
+  //   <div class="headline">{Headline of article}</div>
+  //   <div class="author">
+  //     <div class="img-container">
+  //       <img src={url of authors image} />
+  //     </div>
+  //     <span>By {authors name}</span>
+  //   </div>
+  // </div>
+  // -------------------------------------------------------------
 
-	const card = document.createElement('div');
-	card.classList.add('card');
+  const card = document.createElement('div');
+  card.classList.add('card');
 
-	const headline = document.createElement('div');
-	headline.classList.add('headline');
-	headline.textContent = article.headline;
-	card.appendChild(headline);
+  const headline = document.createElement('div');
+  headline.classList.add('headline');
+  headline.textContent = article.headline;
+  card.appendChild(headline);
 
-	const author = document.createElement('div');
-	author.classList.add('author');
-	card.appendChild(author);
+  const author = document.createElement('div');
+  author.classList.add('author');
+  card.appendChild(author);
 
-	imgContainer = document.createElement('div');
-	imgContainer.classList.add('img-container');
-	author.appendChild(imgContainer); // appends inside of author div.
+  imgContainer = document.createElement('div');
+  imgContainer.classList.add('img-container');
+  author.appendChild(imgContainer); // appends inside of author div.
 
-	const image = document.createElement('img');
-	image.src = article.authorPhoto;
-	imgContainer.appendChild(image); // appends inside of author div inside of img-container.
+  const image = document.createElement('img');
+  image.src = article.authorPhoto;
+  imgContainer.appendChild(image); // appends inside of author div inside of img-container.
 
-	const authorName = document.createElement('span');
-	authorName.textContent = article.authorName;
-	author.appendChild(authorName); // appends inside of aurtho div
+  const authorName = document.createElement('span');
+  authorName.textContent = article.authorName;
+  author.appendChild(authorName); // appends inside of aurtho div
 
-	return card;
+  return card;
 }
